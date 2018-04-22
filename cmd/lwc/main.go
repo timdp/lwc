d712e712d04a1d6703f8923bd1b4af521fb68cd1
Tim De Pauw <timdp@users.noreply.github.com>
2018-04-22 13:03:46 +0200

    Factor out global vars

--- cmd/lwc/main.go
+++ cmd/lwc/main.go
@@ -11,51 +11,53 @@ import (
 	"sync"
 )
 
-var countLines bool
-var countWords bool
-var countChars bool
-var countBytes bool
+type Opts struct {
+	countLines bool
+	countWords bool
+	countChars bool
+	countBytes bool
+}
 
-var wg sync.WaitGroup
-
-func getOpts() {
+func getOpts() Opts {
+	var opts Opts
 	for _, arg := range os.Args {
 		switch {
 		case arg == "-l" || arg == "--lines":
-			countLines = true
+			opts.countLines = true
 		case arg == "-w" || arg == "--words":
-			countWords = true
+			opts.countWords = true
 		case arg == "-m" || arg == "--chars":
-			countChars = true
+			opts.countChars = true
 		case arg == "-c" || arg == "--bytes":
-			countBytes = true
+			opts.countBytes = true
 		}
 	}
-	if !(countLines || countWords || countChars || countBytes) {
-		countLines = true
-		countWords = true
-		countBytes = true
+	if !(opts.countLines || opts.countWords || opts.countChars || opts.countBytes) {
+		opts.countLines = true
+		opts.countWords = true
+		opts.countBytes = true
 	}
+	return opts
 }
 
-func buildSplits() []bufio.SplitFunc {
+func buildSplits(opts Opts) []bufio.SplitFunc {
 	var splits []bufio.SplitFunc
-	if countLines {
+	if opts.countLines {
 		splits = append(splits, bufio.ScanLines)
 	}
-	if countWords {
+	if opts.countWords {
 		splits = append(splits, bufio.ScanWords)
 	}
-	if countChars {
+	if opts.countChars {
 		splits = append(splits, bufio.ScanRunes)
 	}
-	if countBytes {
+	if opts.countBytes {
 		splits = append(splits, bufio.ScanBytes)
 	}
 	return splits
 }
 
-func consumeReader(reader *io.PipeReader, split bufio.SplitFunc, update chan int, i int) {
+func consumeReader(reader *io.PipeReader, split bufio.SplitFunc, update chan int, i int, wg sync.WaitGroup) {
 	defer wg.Done()
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(split)
@@ -70,7 +72,7 @@ func consumeReader(reader *io.PipeReader, split bufio.SplitFunc, update chan int
 	}
 }
 
-func printCounts(numCounts int, update chan int) {
+func printCounts(numCounts int, update chan int, wg sync.WaitGroup) {
 	defer wg.Done()
 	counts := make([]int, numCounts)
 	out := make([]string, numCounts)
@@ -90,7 +92,7 @@ func printCounts(numCounts int, update chan int) {
 	}
 }
 
-func pipeStdin(pws []*io.PipeWriter) {
+func pipeStdin(pws []*io.PipeWriter, wg sync.WaitGroup) {
 	defer wg.Done()
 	numCounts := len(pws)
 	writers := make([]io.Writer, numCounts)
@@ -106,10 +108,10 @@ func pipeStdin(pws []*io.PipeWriter) {
 
 func main() {
 	// Read command-line args
-	getOpts()
+	opts := getOpts()
 
 	// Determine which counters to use
-	splits := buildSplits()
+	splits := buildSplits(opts)
 	numCounts := len(splits)
 
 	// For each counter, set up a pipe for stdin
@@ -122,20 +124,20 @@ func main() {
 	// Set up channel where counters will send updates
 	update := make(chan int)
 
-	// We have a goroutine for each counter, plus one for piping input, plus one
-	// for producing output
+	// Set up WaitGroup for our goroutines
+	var wg sync.WaitGroup
 	wg.Add(numCounts + 2)
 
-	// Listen for updates to counters
-	go printCounts(numCounts, update)
+	// Start listening for updates to counters
+	go printCounts(numCounts, update, wg)
 
 	// Start reading from pipes
 	for i, split := range splits {
-		go consumeReader(prs[i], split, update, i)
+		go consumeReader(prs[i], split, update, i, wg)
 	}
 
-	// Write to pipes
-	go pipeStdin(pws)
+	// Start writing to pipes
+	go pipeStdin(pws, wg)
 
 	// Wait for goroutines to complete
 	wg.Wait()
