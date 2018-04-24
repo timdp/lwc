0a27d41fc478286b9e650f7a395ed563fb810a46
Tim De Pauw <timdp@users.noreply.github.com>
2018-04-24 09:27:16 +0200

    Replace channels with direct pointer updates

--- cmd/lwc/main.go
+++ cmd/lwc/main.go
@@ -21,12 +21,6 @@ type Config struct {
 	files      []string
 }
 
-type Update struct {
-	channel int
-	count   uint64
-	done    bool
-}
-
 const COUNT_FORMAT string = "%8d"
 const UPDATE_INTERVAL time.Duration = 50 * time.Millisecond
 const CARRIAGE_RETURN byte = 13
@@ -86,15 +80,15 @@ func openFile(name string) *os.File {
 	return file
 }
 
-func printCounts(counts []uint64, label string, cr bool) {
+func printCounts(counts *[]uint64, label string, cr bool) {
 	var sb strings.Builder
 	if cr {
 		sb.WriteByte(CARRIAGE_RETURN)
 	}
-	sb.WriteString(fmt.Sprintf(COUNT_FORMAT, counts[0]))
-	for i := 1; i < len(counts); i++ {
+	sb.WriteString(fmt.Sprintf(COUNT_FORMAT, (*counts)[0]))
+	for i := 1; i < len(*counts); i++ {
 		sb.WriteByte(SPACE)
-		sb.WriteString(fmt.Sprintf(COUNT_FORMAT, counts[i]))
+		sb.WriteString(fmt.Sprintf(COUNT_FORMAT, (*counts)[i]))
 	}
 	if label != "" {
 		sb.WriteByte(SPACE)
@@ -103,45 +97,16 @@ func printCounts(counts []uint64, label string, cr bool) {
 	os.Stdout.WriteString(sb.String())
 }
 
-func consumeReader(reader *io.PipeReader, split bufio.SplitFunc, in chan bool, out chan Update, index int, wg *sync.WaitGroup) {
+func consumeReader(reader *io.PipeReader, split bufio.SplitFunc, count *uint64, wg *sync.WaitGroup) {
 	defer wg.Done()
-	var count uint64
-	go func() {
-		for <-in {
-			out <- Update{index, atomic.LoadUint64(&count), false}
-		}
-	}()
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(split)
 	for scanner.Scan() {
 		// fmt.Printf("%v: %v\n", i, scanner.Text())
-		atomic.AddUint64(&count, 1)
+		atomic.AddUint64(count, 1)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
-	} else {
-		out <- Update{index, atomic.LoadUint64(&count), true}
-	}
-}
-
-func collectCounts(name string, numCounts int, totals *[]uint64, updates chan Update, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	counts := make([]uint64, numCounts)
-	// Print zeroes straightaway in case file is empty
-	printCounts(counts, name, false)
-
-	completed := 0
-	for completed < numCounts {
-		update := <-updates
-		counts[update.channel] = update.count
-		if totals != nil {
-			(*totals)[update.channel] = update.count
-		}
-		printCounts(counts, name, true)
-		if update.done {
-			completed++
-		}
 	}
 }
 
@@ -161,6 +126,12 @@ func pipeSource(file *os.File, pws []*io.PipeWriter) {
 func processFile(file *os.File, name string, splits []bufio.SplitFunc, totals *[]uint64) {
 	numCounts := len(splits)
 
+	// Create counters
+	counts := make([]uint64, numCounts)
+
+	// Write zeroes straightaway in case file is empty
+	printCounts(&counts, name, false)
+
 	// For each counter, set up a pipe
 	prs := make([]*io.PipeReader, numCounts)
 	pws := make([]*io.PipeWriter, numCounts)
@@ -168,42 +139,27 @@ func processFile(file *os.File, name string, splits []bufio.SplitFunc, totals *[
 		prs[i], pws[i] = io.Pipe()
 	}
 
-	// Set up channels
-	ins := make([]chan bool, numCounts)
-	for i := range ins {
-		ins[i] = make(chan bool)
-	}
-	out := make(chan Update)
-
-	// Set up WaitGroup for our goroutines
-	var wg sync.WaitGroup
-	wg.Add(numCounts + 1)
-
-	// Request an update at fixed intervals
+	// Update stdout at fixed intervals
 	tick := time.NewTicker(UPDATE_INTERVAL)
 	done := make(chan bool)
 	go func() {
 		for {
 			select {
 			case <-tick.C:
-				for _, in := range ins {
-					in <- true
-				}
+				printCounts(&counts, name, true)
 			case <-done:
-				for _, in := range ins {
-					close(in)
-				}
 				break
 			}
 		}
 	}()
 
-	// Start listening for updates to counters
-	go collectCounts(name, numCounts, totals, out, &wg)
+	// Set up WaitGroup for our goroutines
+	var wg sync.WaitGroup
+	wg.Add(numCounts)
 
 	// Start reading from pipes
 	for index, split := range splits {
-		go consumeReader(prs[index], split, ins[index], out, index, &wg)
+		go consumeReader(prs[index], split, &counts[index], &wg)
 	}
 
 	// Write to pipes
@@ -212,10 +168,12 @@ func processFile(file *os.File, name string, splits []bufio.SplitFunc, totals *[
 	// Wait for goroutines to complete
 	wg.Wait()
 
+	// Stop ticker
 	done <- true
 	tick.Stop()
 
-	// Write final newline
+	// Write final counts
+	printCounts(&counts, name, true)
 	fmt.Println()
 }
 
@@ -248,7 +206,7 @@ func processFiles(files []string, splits []bufio.SplitFunc) {
 
 	// If we were keeping totals, print them now
 	if totals != nil {
-		printCounts(totals, "total\n", false)
+		printCounts(&totals, "total\n", false)
 	}
 }
 
