bc7baaa7484495fb066e84e5d1004d816dae2f80
Tim De Pauw <timdp@users.noreply.github.com>
2018-04-28 13:06:50 +0200

    Check if version exists

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,5 +4,10 @@ set -e
 
 ver="v$1"
 
+if git rev-parse $ver >/dev/null 2>&1; then
+  echo Error: tag $ver already exists >&2
+  exit 1
+fi
+
 git commit --allow-empty -m "$ver"
 git tag -a "$ver" -m "$ver"
