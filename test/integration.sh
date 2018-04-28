19b23ab016229149fe91c802f34ce9ba163ff971
Tim De Pauw <timdp@users.noreply.github.com>
2018-04-28 12:03:13 +0200

    Improve integration test output

--- test/integration.sh
+++ test/integration.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+[ -t 1 ] && TTY=1 || TTY=0
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 LWC="$ROOT/bin/lwc-debug"
 WC="$( which wc )"
@@ -9,15 +10,16 @@ WC="$( which wc )"
 failures=0
 for path in test/fixtures/*; do
   name="$( basename "$path" )"
-  echo -n "[????] $name"
+  [[ $TTY = 1 ]] && echo -n "[????] $name"
   set -- $( "$WC" -l -w -m -c <"$path" )
   wc_counts=$@
   set -- $( "$LWC" -l -w -m -c <"$path" )
   lwc_counts=$@
+  [[ $TTY = 1 ]] && echo -ne "\r"
   if [[ $wc_counts = $lwc_counts ]]; then
-    echo -e "\r[PASS] $name"
+    echo "[PASS] $name"
   else
-    echo -e "\r[FAIL] $name"
+    echo "[FAIL] $name"
     echo "       Expected: $wc_counts"
     echo "       Actual:   $lwc_counts"
     (( failures++ ))
