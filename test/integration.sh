2590a8285d8f17d3cecac55e9fd3c84a3486b1f8
Tim De Pauw <timdp@users.noreply.github.com>
2022-07-26 08:57:23 +0200

    Make tests compatible with macOS

--- test/integration.sh
+++ test/integration.sh
@@ -5,15 +5,18 @@ set -e
 [ -t 1 ] && TTY=1 || TTY=0
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 LWC="$ROOT/bin/lwc"
-WC="$( which wc )"
+WC=$( which wc )
+FLAGS='-l -w -m'
+# Detect GNU
+wc -L </dev/null >/dev/null 2>&1 && FLAGS="$FLAGS -c -L"
 
 failures=0
 for path in test/fixtures/*; do
   name="$( basename "$path" )"
   [[ $TTY = 1 ]] && echo -n "[????] $name"
-  set -- $( "$WC" -l -w -m -c -L <"$path" )
+  set -- $( "$WC" $FLAGS <"$path" )
   wc_counts=$@
-  set -- $( "$LWC" -l -w -m -c -L <"$path" )
+  set -- $( "$LWC" $FLAGS <"$path" )
   lwc_counts=$@
   [[ $TTY = 1 ]] && echo -ne "\r"
   if [[ $wc_counts = $lwc_counts ]]; then
